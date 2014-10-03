071a7e480697d8a246562cd2d254fcd8f4f70d70
Hector <Hector@PC-Hector>
2014-10-03 18:37:05 +0200

    First commit

---
+++ TestC/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Hector
+ *
+ * Created on 3 octobre 2014, 18:36
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
